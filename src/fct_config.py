1a858ca20eb0fdd6063331c44502d4762e82eb84
nsviel <nathan.sanchizviel@pm.me>
2022-04-21 14:30:42 +0200

    commit

--- src/fct_config.py
+++ src/fct_config.py
@@ -99,7 +99,7 @@ def check_if_integer(value):
 def str2bool(v):
     if isinstance(v, bool):
         return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+    if v.lower() in ('yes', 'true', 't', 'y', '1', ''):
         return True
     elif v.lower() in ('no', 'false', 'f', 'n', '0'):
         return False
--- src/fct_display.py
+++ src/fct_display.py
@@ -60,7 +60,7 @@ def show_stat():
 def str2bool(v):
     if isinstance(v, bool):
         return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+    if v.lower() in ('yes', 'true', 't', 'y', '1', ''):
         return True
     elif v.lower() in ('no', 'false', 'f', 'n', '0'):
         return False
