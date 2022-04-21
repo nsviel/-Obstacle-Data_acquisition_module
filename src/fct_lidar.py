f3da95e18f551ff1f2283e91c366ff0e4c390acc
nsviel <nathan.sanchizviel@pm.me>
2022-04-21 14:50:19 +0200

    commit

--- src/fct_lidar.py
+++ src/fct_lidar.py
@@ -12,6 +12,12 @@ def lidar_start_motor():
     print("[\033[92mLID\033[0m] - LiDAR motor activated at \033[96m%d\033[0m rpm" % fct_param.lidar_speed)
     #-------------
 
+    response = requests.get(fct_param.lidar_1_url)
+    if response.status_code != 200:
+        print('HTTP', response.status_code)
+    else:
+        print(response.text)
+
     data = {
         'rpm': str(fct_param.lidar_speed),
     }
