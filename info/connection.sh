00c0ddb099213429e28061c9ddbca5c0a2ec5072
nsviel <nathan.sanchizviel@pm.me>
2022-04-29 14:57:06 +0200

    commit

--- info/connection.sh
+++ info/connection.sh
@@ -10,6 +10,7 @@ ip=192.168.1.221/24
 
 if [[ $connection == *"$substr"* ]];
 then
+    sudo nmcli connection modify $connection_name ipv4.addresses $ip ipv4.method manual ipv6.method disabled
     echo -e "[\e[92mok\e[0m] \e[36m$connection_name\e[0m [\e[36m$ip\e[0m] on device \e[36m$device\e[0m already exists"
 else
     sudo nmcli connection add con-name $connection_name ifname $device type ethernet
@@ -25,6 +26,7 @@ ip=192.168.1.222/22
 
 if [[ $connection == *"$substr"* ]];
 then
+    sudo nmcli connection modify $connection_name ipv4.addresses $ip ipv4.method manual ipv6.method disabled
     echo -e "[\e[92mok\e[0m] \e[36m$connection_name\e[0m [\e[36m$ip\e[0m] on device \e[36m$device\e[0m already exists"
 else
     sudo nmcli connection add con-name $connection_name ifname $device type ethernet
