418f11a7f666d520aa7b32dc7f1d38eba9106682
nsviel <nathan.sanchizviel@pm.me>
2022-06-24 13:44:33 +0200

    commit

--- ssh/ssh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ip_dest=10.201.20.106
-
-echo -e "[\e[92mok\e[0m]" Connection SSH with "[\e[92m$ip_dest\e[0m]"
-ssh -vtCX -o ConnectTimeout=1 ns@$ip_dest 'export MESA_GL_VERSION_OVERRIDE=3.3; bash -l'
-echo -e "[\e[92mok\e[0m]" Connection SSH terminated
-
-
