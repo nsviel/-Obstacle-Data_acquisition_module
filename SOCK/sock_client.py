72b7cbaa5c2d070faa573c4b9e34d2f962909ab9
nsviel <nathan.sanchizviel@pm.me>
2022-07-25 13:55:50 +0200

    commit

--- SOCK/sock_client.py
+++ SOCK/sock_client.py
@@ -7,5 +7,8 @@ from SOCK import sock_client_fct
 def connection():
     sock_client_fct.create_socket()
 
-def send_packet(packet):
-    sock_client_fct.send_packet(packet)
+def send_packet_l1(packet):
+    sock_client_fct.send_packet_l1(packet)
+
+def send_packet_l2(packet):
+    sock_client_fct.send_packet_l2(packet)
--- SOCK/sock_client_fct.py
+++ SOCK/sock_client_fct.py
@@ -10,8 +10,14 @@ def create_socket():
     param_py.sock_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     param_py.sock_client_ok = True
 
-def send_packet(packet):
+def send_packet_l1(packet):
     ip = param_py.state_py["hubium"]["ip"]
     port = param_py.state_py["hubium"]["sock_server_l1_port"]
+    if(packet != None and param_py.sock_client_ok):
+        param_py.sock_client.sendto(packet, (ip, port))
+
+def send_packet_l2(packet):
+    ip = param_py.state_py["hubium"]["ip"]
+    port = param_py.state_py["hubium"]["sock_server_l2_port"]
     if(packet != None and param_py.sock_client_ok):
         param_py.sock_client.sendto(packet, (ip, port))
