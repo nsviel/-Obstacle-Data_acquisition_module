8241dcf8961b5cb9fc8e45a7b4a20dbe0d4b74a3
nsviel <nathan.sanchizviel@pm.me>
2022-09-20 10:55:10 +0200

    truc

--- SOCK/sock_client.py
+++ SOCK/sock_client.py
@@ -6,9 +6,7 @@ def connection():
     sock_client_fct.create_socket()
 
 def send_packet_l1(packet):
-    if(len(packet) == 1248):
-        sock_client_fct.send_packet_l1(packet)
+    sock_client_fct.send_packet_l1(packet)
 
 def send_packet_l2(packet):
-    if(len(packet) == 1248):
-        sock_client_fct.send_packet_l2(packet)
+    sock_client_fct.send_packet_l2(packet)
