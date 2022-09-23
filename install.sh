3b11fe492921079591616a3f22aa0454e30c85b3
nsviel <nathan.sanchizviel@pm.me>
2022-09-23 16:46:00 +0200

    truc

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #/bin/bash!
 
 #Install dependancies
-sudo apt install libiperf0
+sudo apt install -y libiperf0
 sudo python3 -m pip install wheel
 sudo python3 -m pip install pandas
 sudo python3 -m pip install iperf3
 pip install psutil
-
