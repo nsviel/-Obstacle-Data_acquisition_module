f58d832d4665bff82f1145ee8bad1de084640efa
nsviel <nathan.sanchizviel@pm.me>
2023-04-04 17:09:05 +0200

    up

--- src/interface/device.py
+++ src/interface/device.py
@@ -5,7 +5,9 @@ import pcapy
 
 def update_list():
     devices = get_all_device()
+
     cpt = 0
+    param_capture.state_capture["device"].clear()
     for d in devices :
         y = {str(cpt): str(d)}
         param_capture.state_capture["device"].update(y)
@@ -13,25 +15,30 @@ def update_list():
 
 def get_all_device():
     devices = pcapy.findalldevs()
+
+    to_supress = []
     for d in devices :
         if(d == "any"):
-            devices.remove("any")
+            to_supress.append("any")
         elif(d == "lo"):
-            devices.remove("lo")
+            to_supress.append("lo")
         elif(d == "docker0"):
-            devices.remove("docker0")
+            to_supress.append("docker0")
         elif(d == "dbus-system"):
-            devices.remove("dbus-system")
+            to_supress.append("dbus-system")
         elif(d == "dbus-session"):
-            devices.remove("dbus-session")
+            to_supress.append("dbus-session")
         elif(d == "bluetooth0"):
-            devices.remove("bluetooth0")
+            to_supress.append("bluetooth0")
         elif(d == "bluetooth-monitor"):
-            devices.remove("bluetooth-monitor")
+            to_supress.append("bluetooth-monitor")
         elif(d == "nflog"):
-            devices.remove("nflog")
+            to_supress.append("nflog")
         elif(d == "nfqueue"):
-            devices.remove("nfqueue")
+            to_supress.append("nfqueue")
+
+    for d in to_supress :
+        devices.remove(d)
 
     return devices
 
