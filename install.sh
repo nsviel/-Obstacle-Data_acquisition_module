1d5a30bfa7e5b7c06759ae730f2277e1677671da
nsviel <nathan.sanchizviel@pm.me>
2024-01-22 15:26:16 +0100

    work_in_progress

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 #Install dependancies
 sudo apt install -y python3 python3-pip python3-scapy python3-pcapy libiperf0 iputils-ping
-sudo python3 -m pip install scapy requests pandas psutil iperf3 blessings
-pip install psutil
+sudo python3 -m pip install scapy requests pandas psutil iperf3 blessings dpkt
+pip install psutil dpkt
--- src/interface/capture.py
+++ src/interface/capture.py
@@ -11,6 +11,9 @@ import re
 import subprocess
 import os
 import time
+import shlex
+import dpkt
+import tempfile
 
 thread_l2 = None
 thread_l1 = None
@@ -21,7 +24,7 @@ def start_lidar_capture():
     thread_l1 = threading.Thread(target = start_l1_capture)
     thread_l2 = threading.Thread(target = start_l2_capture)
     thread_pcap = threading.Thread(target = start_pcap_capture)
-    #thread_l1.start()
+    thread_l1.start()
     #thread_l2.start()
     thread_pcap.start();
 def stop_lidar_capture():
@@ -42,12 +45,6 @@ def restart_lidar_capture():
 def start_l1_capture():
     dest_port = param_capture.state_edge["hub"]["socket"]["server_l1_port"]
     socket = socket_client.Socket_client()
-
-    # wait for lidar connection
-    connected = False
-    while not connected:
-        connected = param_capture.state_ground["lidar_1"]["info"]["connected"]
-        time.sleep(0.5)
 
     # Check device
     device_ok = check_device(device)
@@ -93,16 +90,6 @@ def start_l2_capture():
         terminal.addDaemon("#", "OFF", "LiDAR 2 capture")
 def start_pcap_capture():
     socket = socket_client.Socket_client()
-
-    # loop check if lidar 1 & lidar 2 are not connected
-    l1_connected = False
-    l2_connected = False
-    while not l1_connected and l2_connected:
-        l1_connected = param_capture.state_ground["lidar_1"]["info"]["connected"]
-        l2_connected = param_capture.state_ground["lidar_2"]["info"]["connected"]
-        time.sleep(0.5)
-
-    # Run pcap reading
     pcap_reader(socket)
 
 def check_device(lidar_device):
@@ -112,12 +99,11 @@ def check_device(lidar_device):
     return device_ok
 def start_capture(socket, lidar_ID, dest_port):
     lidar_device = param_capture.state_ground[lidar_ID]["info"]["device"]
-    simulated = param_capture.state_ground[lidar_ID]["info"]["simulated"]
     connected = param_capture.state_ground[lidar_ID]["info"]["connected"]
     activated = param_capture.state_ground[lidar_ID]["info"]["activated"]
     dest_ip = param_capture.state_edge["hub"]["info"]["ip"]
 
-    if(connected and device_ok and simulated == False):
+    if(connected and device_ok):
         param_capture.state_ground["lidar_1"]["packet"]["value"] = 0
         param_capture.state_ground["lidar_1"]["motor"]["running"] = True
         param_capture.run_thread_l1 = True
@@ -137,8 +123,6 @@ def start_capture(socket, lidar_ID, dest_port):
                         terminal.addCstLog("cap", "%s: [\033[1;32m%s\033[0m] packets"%{lidar_ID, param_capture.state_ground["lidar_2"]["packet"]["value"]})
         terminal.addDaemon("#", "OFF", "%s capture"% lidar_ID)
 def pcap_reader(socket):
-    l1_connected = param_capture.state_ground["lidar_1"]["info"]["connected"]
-    l2_connected = param_capture.state_ground["lidar_2"]["info"]["connected"]
     dest_ip = param_capture.state_edge["hub"]["info"]["ip"]
     dest_port = param_capture.state_edge["hub"]["socket"]["server_l1_port"]
     path = param_capture.path_pcap
@@ -149,46 +133,26 @@ def pcap_reader(socket):
     terminal.addDaemon("#", "ON", "LiDAR 1 pcap")
     param_capture.run_thread_pcap = True
 
-    try:
-        while param_capture.run_thread_pcap:
-            # Run tcpdump and read the packet details, redirecting output to subprocess.PIPE
-            absolute_path = os.path.abspath(path)
-            process = subprocess.Popen(['tcpdump', '-r', absolute_path, '-n', '-v', '-tttt', '-l'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    while param_capture.run_thread_pcap:
+        # Run tcpdump and read the packet details, redirecting output to subprocess.PIPE
+        absolute_path = os.path.abspath(path)
+        command = "tcpdump -r {} -w {}".format(shlex.quote(absolute_path), "aa.dat")
 
-            # Loop over each line (packet) and process it continuously
-            while param_capture.run_thread_pcap:
+        # Run the command and redirect both stdout and stderr to null devices
+        with open(os.devnull, 'w') as null_device:
+            process = subprocess.Popen(command, stdout=null_device, stderr=null_device, shell=True)
+            process.wait()
 
-                # loop check if lidar 1 & lidar 2 are not connected
-                while not l1_connected and l2_connected:
-                    time.sleep(0.5)
+        # Open the temporary pcap file for reading
+        with open("aa.dat", 'rb') as pcap_file:
+            # Loop over each packet and process it continuously
+            pcap = dpkt.pcap.Reader(pcap_file)
 
-                # Read a single line (packet) from the subprocess output
-                line = process.stdout.readline()
-                packet = line.strip()
-                #print(len(packet))
+            for ts, buf in pcap:
+                packet = bytes(buf)
+                socket.socket.sendto(packet, ("localhost", 8308))
 
-                if not packet:
-                    # If there's no more output, break the inner loop
-                    break
-
-                if packet is not None:
-                    socket.socket.sendto(packet.encode(), (dest_ip, dest_port))
-                    if len(packet) == 1248:
-                        param_capture.state_ground["lidar_1"]["packet"]["value"] += 1
-                        terminal.addCstLog("cap", "LiDAR 2: [\033[1;32m%s\033[0m] packets" % param_capture.state_ground["lidar_2"]["packet"]["value"])
-
-            # Ensure the tcpdump process is terminated when the inner loop is done
+            # Wait for the process to complete (optional)
             process.terminate()
-
-            # Reset the file position to the beginning after reaching the end
-            with open(path, 'rb') as file:
-                file.seek(initial_file_size)
-    finally:
-        # Ensure the tcpdump process is terminated when the loop is done
-        process.terminate()
-
-        # Reset the file position to the beginning after reaching the end
-        with open(path, 'rb') as file:
-            file.seek(initial_file_size)
 
     terminal.addDaemon("#", "OFF", "LiDAR pcap")
